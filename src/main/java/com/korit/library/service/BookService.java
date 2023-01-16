015eac9d464b49aba0034fbf0703f0ad7ee0812c
DevPCW <rorndksk123@naver.com>
2023-01-16 21:53:16 +0900

    저녁웹개발: SpringBoot_changwoo 20230116

--- src/main/java/com/korit/library/service/BookService.java
+++ src/main/java/com/korit/library/service/BookService.java
@@ -2,21 +2,26 @@ package com.korit.library.service;
 
 import com.korit.library.exception.CustomValidationException;
 import com.korit.library.repository.BookRepository;
-import com.korit.library.web.dto.BookMstDto;
-import com.korit.library.web.dto.BookReqDto;
-import com.korit.library.web.dto.CategoryDto;
-import com.korit.library.web.dto.SearchReqDto;
+import com.korit.library.web.dto.*;
 import jdk.jfr.Category;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 @Service
 public class BookService {
+
+    @Value("${file.path}") // 스프링 어노테이션 // 지역변수에 적어도 됨
+    private String filePath; // 'yml' 에 들어있는 경로
 
     @Autowired
     private BookRepository bookRepository;
@@ -48,5 +53,53 @@ public class BookService {
 
     public void modifyBook(BookReqDto bookReqDto) {
         bookRepository.updateBookByBookCode(bookReqDto);
+    }
+
+    public void maintainModifyBook(BookReqDto bookReqDto) {
+        bookRepository.maintainUpdateBookByBookCode(bookReqDto);
+    }
+
+    public void deleteBook(String bookCode) {
+        bookRepository.deleteBookByBookCode(bookCode);
+    }
+
+    public void registerBookImages(String bookCode, List<MultipartFile> files) {
+        if(files.size() < 1) { // 1보다 적으면 로직 실행
+            Map<String, String> errorMap = new HashMap<String, String>();
+            errorMap.put("file", "이미지를 선택하세요.");
+
+            throw new CustomValidationException(errorMap);
+        }
+
+        List<BookImageDto> bookImageDtos = new ArrayList<BookImageDto>();
+
+        files.forEach(file -> {
+            String originFileName = file.getOriginalFilename();
+            String extension = originFileName.substring(originFileName.lastIndexOf("."));
+            String tempFileName = UUID.randomUUID().toString().replaceAll("-", "") + extension;
+
+            Path uploadPath = Paths.get(filePath + "/book/" + tempFileName); // java.nio.File
+
+            File f = new File(filePath + "/book"); // Java.io
+            if(!f.exists()) { // 해당 경로가 없으면
+                f.mkdirs(); // 경로 생성
+            }
+
+            try {
+                Files.write(uploadPath, file.getBytes()); // java.nio.File // 여기 경로에다가 복사
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            BookImageDto bookImageDto = BookImageDto.builder()
+                    .bookCode(bookCode)
+                    .saveName(tempFileName)
+                    .originName(originFileName)
+                    .build();
+
+            bookImageDtos.add(bookImageDto);
+        });
+
+        bookRepository.registerBookImages(bookImageDtos);
     }
 }
--- src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -5,12 +5,17 @@ import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.BookService;
 import com.korit.library.web.dto.*;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.ReportAsSingleViolation;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -55,6 +60,41 @@ public class BookApi {
         bookService.modifyBook(bookReqDto);
         return ResponseEntity
                 .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @ValidAspect
+    @PatchMapping("/book/{bookCode}") // 'PatchMapping' 과의 차이점: 쿼리문에서 수정 시 빈 값이 나오면 'Put' => 전부 바꾸겠다 / 'Patch' 는 기본값 유지하면서 수정한 값들만 바꿈
+    public ResponseEntity<CMRespDto<?>> maintainModifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.maintainModifyBook(bookReqDto);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+
+    @ParamsAspect
+    @DeleteMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> deleteBook(@PathVariable String bookCode) {
+        bookService.deleteBook(bookCode);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "files", required = false)
+    })
+    @ParamsAspect
+    @PostMapping("/book/{bookCode}/images")
+    public ResponseEntity<CMRespDto<?>> registerBookImg(@PathVariable String bookCode, @RequestPart List<MultipartFile> files) {
+//        MultipartFile file = files.get(0);
+//        System.out.println(file.getOriginalFilename());
+
+        bookService.registerBookImages(bookCode, files);
+
+        return ResponseEntity.ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
     }
 }
